c27448413218ebdaec0b8c362c9c0d5b2068e879
varoonsahgal <varoon.sahgal@gmail.com>
2026-01-20 20:55:16 -0500

    initial commit

---
+++ src/main/java/com/cleancodehex/ebp/blackjack/Hand.java
@@ -0,0 +1,76 @@
+package com.cleancodehex.ebp.blackjack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.fusesource.jansi.Ansi.ansi;
+
+public class Hand {
+    private final List<Card> cards = new ArrayList<>();
+
+    public Hand(List<Card> cards) {
+        this.cards.addAll(cards);
+    }
+
+    public Hand() {
+    }
+
+    public int value() {
+        int handValue = cards
+                .stream()
+                .mapToInt(Card::rankValue)
+                .sum();
+
+        // does the hand contain at least 1 Ace?
+        boolean hasAce = cards
+                .stream()
+                .anyMatch(card -> card.rankValue() == 1);
+
+        // if the total hand value <= 11, then count the Ace as 11 by adding 10
+        if (hasAce && handValue <= 11) {
+            handValue += 10;
+        }
+
+        return handValue;
+    }
+
+    public String displayFaceUpCard() {
+        return cards.get(0).display();
+    }
+
+    public boolean dealerMustDrawCard() {
+        return value() <= 16;
+    }
+
+    public void display() {
+        System.out.println(cards.stream()
+                                .map(Card::display)
+                                .collect(Collectors.joining(
+                                        ansi().cursorUp(6).cursorRight(1).toString())));
+    }
+
+    public void drawFrom(Deck deck) {
+        cards.add(deck.draw());
+    }
+
+    public boolean isBusted() {
+        return value() > 21;
+    }
+
+    public boolean pushes(Hand hand) {
+        return hand.value() == value();
+    }
+
+    public boolean beats(Hand hand) {
+        return hand.value() < value();
+    }
+
+    public String displayValue() {
+        return String.valueOf(value());
+    }
+
+    public boolean valueEquals(int target) {
+        return value() == target;
+    }
+}
